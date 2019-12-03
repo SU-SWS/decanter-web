7852318b86a3b9d396a73871034d94ebfd0f16bc
Shea McKinney <sherakama@gmail.com>
2019-12-02 17:28:59 -0800

    upsy daisy.

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import { Component } from 'react';
 import Layout from '../components/layouts/OneCol.js';
 import { attributes, react as HomeContent } from '../content/index.md';
 
-const MyContent = [
-  <HomeContent />
-];
-
-export default function Index() {
-  return <Layout content={HomeContent} />;
+export default class Home extends Component {
+  render() {
+    return (
+      <Layout content=<HomeContent />/>
+    )
+  }
 }
