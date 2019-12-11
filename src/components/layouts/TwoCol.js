588544a8be09e43191a70dec79ce766f59a0dff5
Yvonne Tang <ytang2@stanford.edu>
2019-12-11 11:42:56 -0800

    Continue styling homepage and add download button; use grid for sidebar to more netlify logo to bottom

--- src/components/layouts/TwoCol.js
+++ src/components/layouts/TwoCol.js
@@ -13,7 +13,7 @@ const Layout = props => (
   <SkipLink />
   <MastHead />
   <main>
-    <aside>
+    <aside aria-label="left sidebar" id="left-sidebar">
       <Logo />
       <PrimaryNav />
       <a href="https://www.netlify.com">
