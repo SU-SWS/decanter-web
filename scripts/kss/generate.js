6745c5124e18c6125d3511fabae296ce2cc00b0f
Shea McKinney <sherakama@gmail.com>
2019-12-13 13:39:50 -0800

    its working!

--- scripts/kss/generate.js
+++ scripts/kss/generate.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 const del = require('del');
+const Twig = require('twig');
+const twigDrupal = require('twig-drupal-filters');
 const kss_util = require('./utility.js');
 const kss_nav_json = path.resolve(__dirname, '../../content/_settings/kss.json');
 const kss_settings_dir = path.resolve(__dirname, '../../content/_kss');
 const decanter_src = path.resolve(__dirname, "../../node_modules/decanter/core/src");
+
+// Add de filters.
+twigDrupal(Twig);
 
 /**
  * Generate the navigation json.
@@ -44,19 +49,44 @@ kss_util.fetchSections()
  * @type {[type]}
  */
 del.sync([kss_settings_dir + 'data/**', '!' + kss_settings_dir + "data"]);
+del.sync([kss_settings_dir + 'markup/**', '!' + kss_settings_dir + "markup"]);
 kss_util.fetchSections()
   .then(function(sections) {
-    Object.keys(sections).forEach(function(key) {
+    Object.keys(sections).forEach(async function(key) {
       var section = sections[key];
       var markup = section.markup();
       if (markup.match(/(<|>)/g)) {
         return;
       }
-      var short_path = markup.trim().replace(/\.\.\//ig, '').replace(".twig", ".json");
+      var twig_path = markup.trim().replace(/\.\.\//ig, '');
+      var full_twig_path = path.resolve(decanter_src, twig_path);
+      var short_path = twig_path.replace(".twig", ".json");
       var full_path = path.join(decanter_src, short_path);
       var dest_path = path.join(kss_settings_dir, 'data', key + '.json');
+
+      // Save the data to file.
       fs.copyFile(full_path, dest_path, (err) => {
         if (err) console.log(err);
+      });
+
+      var contents = fs.readFileSync(full_path);
+      var schema = JSON.parse(contents);
+      var markup = "";
+      schema.modifier_class = "[ modifier_class ]";
+
+      var options = {
+        ...schema,
+        settings: {
+          'twig options': {
+            namespaces: { decanter: path.join(decanter_src, "templates/") },
+            allowAsync: false
+          }
+        }
+      };
+
+      Twig.renderFile(full_twig_path, options, function(err, html) {
+        let filepath = path.join(kss_settings_dir, "markup", key + ".html");
+        fs.writeFileSync(filepath, html);
       });
     });
   }
