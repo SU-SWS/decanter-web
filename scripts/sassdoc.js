9fe8550425b93c2c800f8b57a017cf97b5a3c1e5
Shea McKinney <sherakama@gmail.com>
2019-12-19 01:19:32 -0800

    lightly themed sassdocs.

--- scripts/sassdoc.js
+++ scripts/sassdoc.js
@@ -1,5 +1,6 @@
 var sassdoc = require('sassdoc');
 var opts = {
+  "theme": "./sassdoc-theme",
   "package": "./node_modules/decanter/package.json",
   "src": "./node_modules/decanter/core/src/scss/",
   "dest": "./public/docs/scss"
