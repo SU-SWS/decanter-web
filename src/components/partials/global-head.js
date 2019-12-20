c76977303b808651fa19a766a69e601aeeccb89e
Dena DeBry <dena@buttonwillowsix.com>
2019-12-20 11:58:03 -0700

    Update global-head.js with GA
    Added Google Analytics to the head

--- src/components/partials/global-head.js
+++ src/components/partials/global-head.js
@@ -5,6 +5,15 @@ const HTMLHead = props => (
   <>
   <Head>
     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130026260-16"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-130026260-16');
+      </script>
   </Head>
   </>
 );
