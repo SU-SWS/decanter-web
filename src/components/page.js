d4dcc4235058282646c4049b1efb7444598048ff
Shea McKinney <sherakama@gmail.com>
2020-10-08 22:48:23 -0700

    Smol changes.

--- src/components/page.js
+++ src/components/page.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Components from './components.js';
+import Layout from './layout';
 
 const Page = (props) => (
-  <div className="page">
-    {props.blok.body && props.blok.body.map((blok) => React.createElement(Components(blok.component),
-      {key: blok._uid,
-        blok: blok
-      }))}
-  </div>
+  <Layout className="page">
+    {props.content}
+  </Layout>
 )
 
 export default Page
