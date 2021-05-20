8a05d3e2db604005ef0731ad25850b851c44e14e
Shea McKinney <sherakama@gmail.com>
2021-05-19 22:25:03 -0700

    fixup! typo.

---
+++ src/pages/contribute/technical.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BasicPage from "../../components/pageTypes/basicPage";
+
+const ContributeTechnicalPage = () => (
+  <BasicPage>
+    <h1>Technical Contribution Guidelines</h1>
+  </BasicPage>
+);
+
+export default ContributeTechnicalPage;
