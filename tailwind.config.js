9647e524e1f7d605f88aa1d6b2e18dda779b9ba5
Jacob Wilson <49299083+jdwjdwjdw@users.noreply.github.com>
2022-05-12 13:27:48 -0700

    Decanter v7 nextjs setup (#98)
    * ADAPT-4784: Remove gatsby files, initialize nextjs app
    
    * Add tailwind and decanter support

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  presets: [
+    require('decanter')
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
