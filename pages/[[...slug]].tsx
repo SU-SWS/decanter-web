71e4a426f654b3098ab3edd668af1af33ce0ad89
Jacob Wilson <49299083+jdwjdwjdw@users.noreply.github.com>
2022-07-19 14:03:47 -0700

    Feature/adapt 4787: layout styling (#112)
    * Adjust layout styling
    
    * Add global footer
    
    * Adjust responsive spacing, fix link inline-block issue
    
    * Adjust h1 styles

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -112,7 +112,7 @@ const Home: NextPage<{ page: Page }> = ({ page }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <header>
-        <div className='su-cc su-identity-bar su-pt-5 su-pb-3 su-bg-cardinal-red'>
+        <div className='su-identity-bar su-pt-5 su-pb-3 su-pl-26 su-bg-digital-red'>
           <a
             className='su-logo su-text-white hocus:su-text-white su-text-20 su-leading-none'
             href='https://www.stanford.edu'
@@ -121,27 +121,108 @@ const Home: NextPage<{ page: Page }> = ({ page }) => {
           </a>
         </div>
       </header>
-      <div className='su-grid su-grid-gap su-grid-cols-12 su-cc su-rs-pb-8 su-rs-pt-2'>
-        <div className='basic-page-left-sidebar su-col-span-12 lg:su-col-span-4 2xl:su-col-span-3 su-basefont-21 su-relative'>
-          <Container as="aside" width="full" className='su-absolute su-top-0 su-left-0 su-sticky'>
+      <div className='su-grid su-grid-cols-12'>
+        <div className='basic-page-left-sidebar su-col-span-12 lg:su-col-span-3 2xl:su-col-span-2 su-basefont-20 su-relative su-bg-black'>
+          <Container as="aside" width="full" >
             <Link href='/'>
-              <a className="su-font-bold su-type-4 xl:su-type-5 su-no-underline hocus:su-underline">Decanter V7</a>
+              <a className="su-font-bold su-type-3 su-pl-26 su-py-30 su-no-underline su-text-white hocus:su-text-white su-inline-block">Decanter V7</a>
             </Link>
             <VerticalNav menu={menuTree} className="" activeClasses="" showNestedLevels={false} />
           </Container>
         </div>
-        <div className='basic-page-main-content su-col-span-12 lg:su-col-span-8 2xl:su-col-start-5 su-basefont-23 su-ml-0'>
+        <div className='basic-page-main-content su-col-span-12 lg:su-col-span-9 2xl:su-col-span-10 su-basefont-23 su-ml-0'>
           <Container as="main" width="full">
             <Container as="article" width="full">
               <div className='su-mb-8'>
-                <h1 className="su-type-4 xl:su-type-5">{page.title}</h1>
-                <MdxBody components={{ Counter }} />
+                <h1 className="su-type-3 su-cc 2xl:su-px-80 su-rs-py-3 su-mb-0 su-bg-black-90 su-text-white">{page.title}</h1>
+                <Container className='2xl:su-px-80 su-rs-pt-4 su-rs-pb-8'>
+                  <MdxBody components={{ Counter }} />
+                </Container>
               </div>
               {/* <div dangerouslySetInnerHTML={{ __html: page.body.html }} /> */}
             </Container>
           </Container>
         </div>
       </div>
+      <section className="su-global-footer su-basefont-21 su-bg-digital-red su-rs-py-0 su-text-white su-link-white">
+        <div className="su-cc su-flex su-flex-col lg:su-flex-row" title="Common Stanford resources">
+          <div className="su-text-center su-mt-5 su-mb-9">
+            <a className="su-logo su-type-3 hocus:su-text-white" href="https://www.stanford.edu">Stanford<br/>University</a>
+          </div>
+          <div className="lg:su-pl-45 xl:su-pl-50 su-text-left sm:su-text-center lg:su-text-left su-flex-grow">
+            <nav aria-label="global footer menu" className="su-flex su-flex-row sm:su-flex-col su-justify-center sm:su-items-center lg:su-items-start su-mb-10 su-link-no-underline">
+              <ul className="su-list-unstyled su-mb-10 sm:su-mb-4 su-mr-19 sm:su-mr-0 su-p-0 su-text-15 md:su-text-17 2xl:su-text-18 su-flex su-flex-col sm:su-flex-row">
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://www.stanford.edu" className="hover:su-underline focus:su-underline">
+                    Stanford Home
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://visit.stanford.edu/plan/" className="hover:su-underline focus:su-underline">
+                    Maps &amp; Directions
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://www.stanford.edu/search/" className="hover:su-underline focus:su-underline">
+                    Search Stanford
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://emergency.stanford.edu" className="hover:su-underline focus:su-underline">
+                    Emergency Info
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+              </ul>
+              <ul className="su-list-unstyled su-mb-10 sm:su-mb-0 su-ml-19 sm:su-ml-0 su-p-0 su-text-15 sm:su-text-14 md:su-text-15 xl:su-text-16 su-flex su-flex-col sm:su-flex-row sm:su-link-regular">
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://www.stanford.edu/site/terms/" title="Terms of use for sites" className="hover:su-underline focus:su-underline">
+                    Terms of Use
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://www.stanford.edu/site/privacy/" title="Privacy and cookie policy" className="hover:su-underline focus:su-underline">
+                    Privacy
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://uit.stanford.edu/security/copyright-infringement" title="Report alleged copyright infringement" className="hover:su-underline focus:su-underline">
+                    Copyright
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://adminguide.stanford.edu/chapter-1/subchapter-5/policy-1-5-4" title="Ownership and use of Stanford trademarks and images" className="hover:su-underline focus:su-underline">
+                    Trademarks
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li className="sm:su-mr-10 md:su-mr-20 lg:su-mr-27">
+                  <a href="https://studentservices.stanford.edu/more-resources/student-policies/non-academic/non-discrimination" title="Non-discrimination policy" className="hover:su-underline focus:su-underline">
+                    Non-Discrimination
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.stanford.edu/site/accessibility" title="Report web accessibility issues" className="hover:su-underline focus:su-underline">
+                    Accessibility
+                    <span className="su-sr-only">(link is external)</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="su-text-13 sm:su-text-14 su-text-center lg:su-text-left">
+              <span className="su-whitespace-no-wrap">© Stanford University.</span>
+              <span className="su-whitespace-no-wrap">&nbsp; Stanford, California 94305.</span>
+            </div>
+          </div>
+        </div>
+      </section>
     </Container>
   );
 };
