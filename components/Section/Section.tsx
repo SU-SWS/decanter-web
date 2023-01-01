10f0d2dc736297b764c393f72c51539cb34fe34f
Yvonne Tang <ytang2@stanford.edu>
2023-01-01 12:32:01 -0800

    more content; fixup class to className in mdx files

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -5,25 +5,28 @@ import { Heading, HeadingType } from 'components/Typography';
 /**
  * Simple section component for used in content markdown files
  */
-export interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface SectionProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'className'> {
   heading?: string;
   headingLevel?: HeadingType;
+  headingSize?: 1 | 2 | 3;
   id?: string;
   width?: 'default' | 'full';
   children?: React.ReactNode;
+  className?: string;
 }
 
 export const Section = ({
   heading,
   headingLevel = 'h2',
+  headingSize = 2,
   id,
   width = 'default',
   className,
   children
 }: SectionProps) => (
-  <section id={id} className={dcnb('su-rs-mb-5 last:su-mb-0 su-wysiwyg', className)}>
+  <section id={id} className={dcnb('last:su-mb-0 su-wysiwyg', headingLevel === 'h2' ? 'su-rs-mb-5' : '', className)}>
     {heading && (
-      <Heading as={headingLevel} size={2} className="su-max-w-900">
+      <Heading as={headingLevel} size={headingSize} className="su-max-w-900">
         {heading} {id && <a href={`#${id}`} className="su-text-black-40">#</a>}
       </Heading>
     )}
