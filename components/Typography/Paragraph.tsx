2e487c43d9e063d5b27fec0a21c6efbbcc17879a
Yvonne Tang <ytang2@stanford.edu>
2023-01-02 01:47:09 -0800

    add tsc script and fix type error

--- components/Typography/Paragraph.tsx
+++ components/Typography/Paragraph.tsx
@@ -4,4 +4,4 @@ import { Text, TypographyProps } from './Text';
 export type ParagraphProps = TypographyProps & React.HTMLAttributes<HTMLParagraphElement>;
 
 // Convenience component for paragraphs
-export const Paragraph = (props: ParagraphProps) => <Text variant="ma-body" {...props} as="p" />;
+export const Paragraph = (props: ParagraphProps) => <Text {...props} as="p" />;
