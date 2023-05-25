b4d2954b02d79205238ddcd348e0b6fe1d1ea7d5
Elena <elena.cracea@gmail.com>
2023-05-25 10:55:15 -0600

    ADAPTDCTR-27: Remove saa styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,43 +9,7 @@ module.exports = {
     require('decanter'),
   ],
   theme: {
-    extend: {
-      colors: {
-        black: {
-          opacity: {
-            80: 'rgba(46, 45, 41, 80%)', // For trip card date block
-          },
-          'true-opacity': {
-            20: 'rgba(0, 0, 0, 20%)', // For use in linear gradients
-          },
-          '30-opacity': {
-            40: 'rgba(192, 192, 191, 40%)', // For card borders
-          },
-        },
-        'masthead-black': {
-          top: 'rgba(13, 18, 17, 0.95)',
-          bottom: 'rgba(26, 31, 30, 0.85)',
-        },
-        'saa-black': {
-          DEFAULT: '#181D1C',
-          dark: '#070B0A',
-          opacity: {
-            0: 'rgba(24, 29, 28, 0)',
-            30: 'rgba(24, 29, 28, 0.3)',
-            80: 'rgba(24, 29, 28, 0.8)',
-          },
-        },
-        'digital-red-xlight': '#F83535', // Passed contrast test for black background
-        'cardinal-red': {
-          xdark: '#7A0000', // Passed contrast test with digital-red-xlight as text
-          xxdark: '#541107', // Used for hover/focus color for xdark
-        },
-        'saa-electric-blue': {
-          DEFAULT: '#505EEC',
-          light: '#6B77F5',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
