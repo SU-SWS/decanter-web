b4453e673469ff7e350976c61c3f41ad2374e2d6
jdwjdwjdw <jacobdeanwilson@gmail.com>
2024-04-16 13:25:58 -0700

    A11y fixups

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
