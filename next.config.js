13cf19489a604c7f156b759c0446f8ba903bc235
jdwjdwjdw <jacobdeanwilson@gmail.com>
2024-04-17 14:20:19 -0700

    i18n fixup

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
