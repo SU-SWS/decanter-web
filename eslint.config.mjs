b091b5f3ea975edd6dd7ee33d2d31983ba704855
Sherakama <sherakama@gmail.com>
2026-09-08 22:17:42 -0700

    Discard changes to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,5 @@ import nextTypeScript from 'eslint-config-next/typescript';
 export default defineConfig([
   ...nextVitals,
   ...nextTypeScript,
-  globalIgnores(['.contentlayer/**', '.next/**', 'out/**', 'node_modules/**']),
+  globalIgnores(['.next/**', 'out/**', 'node_modules/**']),
 ]);
